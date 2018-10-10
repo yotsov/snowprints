a68854405c19d2f1562a0493178894ff9babcc2a
Deyan Yotsov <deyan@yotsov.org>
2018-10-10 20:06:44 +0200

    compojure set up

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject org.yotsov/snowprints ""
   ; lein -v -> Leiningen 2.8.1 on Java 1.8.0_51 Java HotSpot(TM) 64-Bit Server VM
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :aot :all
   :main snowprints.backend
   ; rm -rf target; lein uberjar; java -Xms1G -Xmx1G -jar target/snowprints--standalone.jar
--- src/snowprints/backend.clj
+++ src/snowprints/backend.clj
@@ -1,5 +1,18 @@
 (ns snowprints.backend
+  (:require [compojure.core :as c]
+            [compojure.handler :as handler]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
+(c/defroutes app-routes
+             (c/GET "/" [] "<h1>Hello World</h1>"))
+
+(def app
+  (handler/site app-routes))
+
+(defn start-app []
+  (jetty/run-jetty app {:port 3000}))
+
 (defn -main []
-  (println "JAR application started."))
+  (println "JAR application started.")
+  (start-app))
